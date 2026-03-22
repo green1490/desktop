c8dd56c81c275c22585a79a54dca0ac5403c6fd2
green1490 <zambokrisztian1490@gmail.com>
2026-03-22 19:47:19 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      # follows channel version
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      rebecca = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.green1490 = ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
