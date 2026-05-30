e46e90d725dbf4303a6b84c79c703019c9c938a3
green1490 <zambokrisztian1490@gmail.com>
2026-05-30 12:50:04 +0200

    replace notification daemon

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     dunst
+     mako
      pipewire
      wireplumber
   ];
